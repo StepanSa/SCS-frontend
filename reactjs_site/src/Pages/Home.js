0b4c3cd2fbc938e2f3bb0fc29750e96c12f2cefe
bohdandr <bohdandr12@gmail.com>
2022-12-02 00:35:40 +0200

    finishedV1

--- reactjs_site/src/Pages/Home.js
+++ reactjs_site/src/Pages/Home.js
@@ -3,19 +3,36 @@ import { Form } from "react-bootstrap"
 import CarouselBox from "../Components/CarouselBox"
 import Popover from "../Components/Popover.js"
 import PopUp from "./PopUp.js"
-import { Container, Row, Col, Card, Button } from 'react-bootstrap';
+import { Container, Row, Col, Modal, Card, Button } from 'react-bootstrap';
 import image1 from '../assets/basket1.jpg'
 import image2 from '../assets/basket2.jpg'
-// import { Text, StyleSheet } from 'react-native-nested-text';
+// import { useRef } from "react";
+import { Link } from "react-scroll"
+// import ScrollToTop from "../Components/ScrollToTop";
+import findphoto from '../assets/footbasketvolley.png'
+import chatphoto from '../assets/signUpLogIn.png'
+
+import { useRef } from "react";
+import ScrollToTop from "../Components/ScrollToTop";
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
 
 
-export default class Home extends Component {
-    render() {
-        return (
-            <>
+function Components() {
+    const find_ = useRef(null);
+    const chat_ = useRef(null);
+    const play_ = useRef(null);
+
+    const scrollToSection = (elementRef) => {
+        window.scrollTo({
+            top: elementRef.current.offsetTop,
+            behavior: "smooth",
+        });
+    };
+    return (
+        <>
+            <section className="backcontainer">
                 <section className="main-container">
                     <Container>
-                        {/* <CarouselBox /> */}
                         <div className="text-center">
 
                             <h3 className="mb-2" style={{ color: "white", margin: 55 }} >The first</h3>
@@ -114,10 +131,178 @@ export default class Home extends Component {
                         </div>
                     </div> */}
                         {/* <Popover /> */}
-                        <PopUp />
+                        {/* <li><Link to='/first-step' className = 'active' smooth={true} duration={500} style={{width:"16rem",color: "pink"}} variant="outline-danger">Find</Link></li>  */}
+                        {/* <Button onClick={() => scrollToSection(find_)} className="link"><>Find</></Button> */}
+                        <Container className='text-center' style={{ paddingTop: '14rem', paddingBottom: '2rem' }}>
+                            <ButtonGroup aria-label="Basic example">
+                                {/* <li><Link to='first-step' className = 'active' smooth={true} duration={500} style={{width:"16rem",color: "pink"}} variant="outline-danger">Find</Link></li> */}
+                                <Button onClick={() => scrollToSection(find_)} style={{ width: "16rem", color: "pink" }} variant="outline-danger">Find</Button>
+                                <Button onClick={() => scrollToSection(chat_)} style={{ width: "16rem", color: "pink" }} variant="outline-danger">Chat</Button>
+                                <Button onClick={() => scrollToSection(play_)} style={{ width: "16rem", color: "pink" }} variant="outline-danger">Play</Button>
+                            </ButtonGroup>
+                            <Modal>
+                                <Modal.Header>
+                                    <Modal.Title className='text-center'>Find</Modal.Title>
+                                </Modal.Header>
+                                <Modal.Body>
+                                    <Form>
+
+                                    </Form>
+                                </Modal.Body>
+                            </Modal>
+
+                            <Modal>
+                                <Modal.Header>
+                                    <Modal.Title className='text-center'>Chat</Modal.Title>
+                                </Modal.Header>
+                                <Modal.Body>
+                                    <Form>
+
+                                    </Form>
+                                </Modal.Body>
+                            </Modal>
+
+                            <Modal>
+                                <Modal.Footer>
+                                    <Modal.Title className='text-center'>Play</Modal.Title>
+                                </Modal.Footer>
+                                <Modal.Body>
+                                    <Form>
+
+                                    </Form>
+                                </Modal.Body>
+                            </Modal>
+                        </Container>
                     </Container>
                 </section>
-            </>
-        )
-    }
+                <section className="border"></section>
+                <section ref={find_} className="find_">
+                    <Container>
+                        <Row>
+                            <Col>
+                                <Container>
+                                    <div>
+
+                                        <h1 align="left" className="mb-2" style={{ fontSize: 75, color: "white" }} >Find</h1>
+
+                                        <h3 style={{ paddingTop: "1.2em" }} align="left">
+                                            <em style={{ fontSize: 33, color: "orange" }}>To start using our site, click on</em>
+                                        </h3>
+                                        <h3 align="left">
+                                            <em style={{ fontSize: 33, color: "orange" }}>the top panel of the screen to</em>
+                                        </h3>
+                                        <h3 align="left">
+                                            <em style={{ fontSize: 33, color: "orange" }}>select a sport</em>
+                                        </h3>
+                                    </div>
+                                </Container>
+                            </Col>
+                            <Col style={{ paddingLeft: "16em" }}>
+                                <Container>
+                                    <div style={{ paddingTop: "8em" }}>
+                                        <img style={{ borderRadius: "2px" }} src={findphoto} width="400" height="95" />
+                                    </div>
+                                </Container>
+                            </Col>
+                        </Row>
+                        <Row style={{ paddingTop: "13em" }} >
+                            <Container>
+                                <Col style={{ paddingLeft: "45em" }}>
+                                    <h3>
+                                        <em style={{ color: "orange", fontSize: 33 }}>Then you will be able to find different</em>
+                                    </h3>
+                                    <h3>
+                                        <em style={{ color: "orange", fontSize: 33 }}>locations on chosen sport</em>
+                                    </h3>
+                                </Col>
+                            </Container>
+                        </Row>
+                    </Container>
+                </section>
+                <section className="border"></section>
+                <section ref={chat_} className="chat_">
+                    <Container>
+                        <Row>
+                            <Col>
+                                <Container>
+                                    <div style={{ paddingLeft: "3em" }}>
+
+                                        <h1 align="right" className="mb-2" style={{ fontSize: 75, color: "white", paddingLeft: "12em" }} >Chat</h1>
+
+                                    </div>
+                                </Container>
+                            </Col>
+                            <Col style={{ paddingLeft: "13em" }}>
+                                <Container>
+                                    <div style={{ paddingTop: "3em" }}>
+                                        <img src={chatphoto} width="250" height="90" />
+                                    </div>
+                                </Container>
+                            </Col>
+                            <Col style={{ paddingLeft: "5em" }}>
+                                <Container>
+                                    <div style={{ paddingTop: "1.2em" }}>
+                                        <h3 align="right">
+                                            <em style={{ fontSize: 38, color: "white" }}>To start chatting, log in.</em>
+                                        </h3>
+                                        <h3 align="right">
+                                            <em style={{ fontSize: 32, color: "orange" }}>Login button is in the upper</em>
+                                        </h3>
+                                        <h3 align="right">
+                                            <em style={{ fontSize: 32, color: "orange" }}>right corner of the screen</em>
+                                        </h3>
+                                    </div>
+                                </Container>
+                            </Col>
+                        </Row>
+                        <Row style={{ paddingTop: "13em" }} >
+                            <Col>
+                                <Container>
+                                    <h3>
+                                        <em style={{ color: "orange", fontSize: 32 }}>Now you can leave message in</em>
+                                    </h3>
+                                    <h3>
+                                        <em style={{ color: "orange", fontSize: 32 }}>telegram-chat of chosen</em>
+                                    </h3>
+                                    <h3>
+                                        <em style={{ color: "orange", fontSize: 32 }}>location</em>
+                                    </h3>
+                                </Container>
+                            </Col>
+                        </Row>
+                    </Container>
+                </section>
+                <section className="border"></section>
+                <section ref={play_} className="play_">
+                    <Container>
+                                <Container>
+                                    <div>
+
+                                        <h1 className="mb-2" style={{ fontSize: 75, color: "white" }} >Play</h1>
+                                    </div>
+                                    <div style={{ paddingTop: "7em" }}>
+                                        <h3 className="text-center">
+                                            <em style={{ fontSize: 40, color: "orange" }}>Congratulations, you have</em>
+                                        </h3>
+                                        <h3 className="text-center">
+                                            <em style={{ fontSize: 40, color: "orange" }}>familiarized yourself with the</em>
+                                        </h3>
+                                        <h3 className="text-center">
+                                            <em style={{ fontSize: 40, color: "orange" }}>features of our site, successfully</em>
+                                        </h3>
+                                        <h3 className="text-center">
+                                            <em style={{ fontSize: 40, color: "orange" }}>registered and found your first sport</em>
+                                        </h3>
+                                        <h3 className="text-center">
+                                            <em style={{ fontSize: 40, color: "orange" }}>Enjoy!</em>
+                                        </h3>
+                                    </div>
+                                </Container>
+                    </Container>
+                </section>
+            </section>
+        </>
+    )
 }
+
+export default Components;
--- reactjs_site/src/Pages/PopUp.js
+++ reactjs_site/src/Pages/PopUp.js
@@ -3,16 +3,28 @@ import { useState } from 'react';
 import { Container, Nav, Navbar, Form, Button, ModalTitle, Modal, Toast, ToastContainer } from 'react-bootstrap';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import {Link} from "react-scroll"
+import { useRef } from "react";
+import ScrollToTop from "../Components/ScrollToTop";
 
 function PopUp() {
+    const find_ = useRef(null);
+    const chat_ = useRef(null);
+    const play_ = useRef(null);
+
+    const scrollToSection = (elementRef) => {
+        window.scrollTo({
+            top: elementRef.current.offsetTop,
+            behavior: "smooth",
+        });
+    };
     return (
         <>
-            <Container className='text-center' style={{ paddingTop: '15.5rem', paddingBottom: '2rem' }}>
+            <Container className='text-center' style={{ paddingTop: '14rem', paddingBottom: '2rem' }}>
                 <ButtonGroup aria-label="Basic example">
                     {/* <li><Link to='first-step' className = 'active' smooth={true} duration={500} style={{width:"16rem",color: "pink"}} variant="outline-danger">Find</Link></li> */}
-                    <Button style={{width:"16rem",color: "pink"}} variant="outline-danger">Find</Button>
-                    <Button style={{width:"16rem",color: "pink"}} variant="outline-danger">Chat</Button>
-                    <Button style={{width:"16rem",color: "pink"}} variant="outline-danger">Play</Button>
+                    <Button onClick={() => scrollToSection(find_)} style={{width:"16rem",color: "pink"}} variant="outline-danger">Find</Button>
+                    <Button onClick={() => scrollToSection(chat_)} style={{width:"16rem",color: "pink"}} variant="outline-danger">Chat</Button>
+                    <Button onClick={() => scrollToSection(play_)} style={{width:"16rem",color: "pink"}} variant="outline-danger">Play</Button>
                 </ButtonGroup>
                 <Modal>
                     <Modal.Header>
