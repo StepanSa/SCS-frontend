ee603ad4b992b5a593179e4d1efcce636a2891a0
Stepan Sarabun <stepankolv12@gmail.com>
2022-12-16 14:48:00 +0200

    Landed contact us page

--- reactjs_site/src/Pages/Contacts.js
+++ reactjs_site/src/Pages/Contacts.js
@@ -1,28 +1,50 @@
 import React, { Component } from "react"
 import { Form, Button, Container } from 'react-bootstrap'
+import { Link } from "react-router-dom";
+
+const ButtonMailto = ({ mailto, label }) => {
+    return (
+        <Link
+            to='#'
+            onClick={(e) => {
+                window.location.href = mailto;
+                e.preventDefault();
+            }}
+        >
+            {label}
+        </Link>
+    );
+};
+
 
 export default class Contacts extends Component {
     render() {
         return (
-            <Container style={{ width: '500px' }}>
-                <h1 className="text-center">Contact us</h1>
-                <Form>
-                    <Form.Group controlId="formBasicEmail">
-                        <Form.Label>Email Adress</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
-                        <Form.Text className="text-muted">We'll never share your email with anyone else</Form.Text>
+            <Container style={{ width: '1000px' }}>
+                <div className="text-center">
 
-                    </Form.Group>
-                    <Form.Group controlId="formBasicPassword">
-                        <Form.Label>Example textarea</Form.Label>
-                        <Form.Control as="textarea" rows="3" />
-                    </Form.Group>
+                    <h4 className="mb-2" style={{ color: "black", margin: 45 }}> Any troubles?</h4>
 
-                    <Form.Group controlId="formBasicCheckbox">
-                        <Form.Check type="checkbox" label="Check me out" />
-                    </Form.Group>
-                    <Button variants="primary" type="submit" className="text-center">Submit</Button>
-                </Form>
+                    <h4 className="mb-2" style={{ color: "black", margin: 25 }}> Found new location?</h4>
+
+                    <h4 className="mb-2" style={{ color: "black", margin: 25 }}> Let us know</h4>
+
+                    <div style={{ display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "space-evenly", marginTop: 50 }}>
+
+                        <div style={{ margin: 30, padding: 30, display: "flex", alignItems: "center", justifyContent: "space-evenly", color: "black", border: "1px solid #e9ebed", width: 400, backgroundColor: "#fff" }}>
+                            <h4 className="mb-2" style={{ display: "flex", color: "black" }}> Email</h4>
+                            <h4 className="mb-2" style={{ display: "flex", color: "black", fontSize: 20, marginLeft: 25 }}>
+                                <ButtonMailto label="scsprojectbeb@gmail.com" mailto="mailto:scsprojectbeb@gmail.com" />
+                            </h4>
+                        </div>
+
+                        <div style={{ padding: 30, display: "flex", alignItems: "center", justifyContent: "space-evenly", color: "black", border: "1px solid #e9ebed", width: 400, backgroundColor: "#fff" }}>
+                            <h4 className="mb-2" style={{ color: "black", textAlign: "center" }}> <a href="https://t.me/+n-hjdV5-9Nw0NmYy">Telegram</a></h4>
+                        </div>
+
+                    </div>
+
+                </div>
             </Container>
         )
     }
